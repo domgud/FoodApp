15bdf4903a6b41ac60b43aaea75dcd3d3580748a
rapsas <paulauskasmykolas21@gmail.com>
2021-05-15 20:01:44 +0300

    Refactoring
    to match magicDraw

--- FoodApp/Controllers/CartController.cs
+++ FoodApp/Controllers/CartController.cs
@@ -32,11 +32,11 @@ namespace FoodApp.Controllers
         [Authorize(Roles = "User")]
         public async Task<IActionResult> AddToCart(int id)
         {
-
             List<int> stuff = HttpContext.Session.Get<List<int>>("cart");
             stuff.Add(id);
             HttpContext.Session.Set<List<int>>("cart", stuff);
-            _flashMessage.Confirmation("added to cart successfully!", _context.Dish.FindAsync(id).Result.Name);
+            var temp = await _context.Dish.FindAsync(id);
+            _flashMessage.Confirmation("added to cart successfully!", temp.Name);
             return Redirect(Request.Headers["Referer"].ToString());
         }
         private async Task<List<CartViewModel>> CalculatePrice(List<int> dishIds)
@@ -47,30 +47,40 @@ namespace FoodApp.Controllers
             decimal totalPrice = countedDishes.Sum(item => _context.Dish.Find(item.id).Price * item.count);
 
             //communicating with the api :^)
-            string restaurant = _context.Restaurant.FindAsync(_context.Dish.Find(dishIds.First()).RestaurantId).Result.Address;
-            var id = User.FindFirstValue(ClaimTypes.NameIdentifier);
-            string client = _context.Client.FindAsync(id).Result.Address;
-            var distance = await _distanceProcessor.LoadDistance(client, restaurant);
-            double distanceceValue = distance.rows.First().elements.First().distance.value;
-            double deliveryfee = distanceceValue / 1000 / 4;
-            //check if the distance is greater than 25 km
-            //then apply additional fee
-            if (distanceceValue > 25000)
+            string restaurant = getRestaurantAddress(dishIds);
+            string client = getUserAddress();
+            double distanceceValue = await checkDistance(client, restaurant);
+            double deliveryfee;
+
+            if (checkIfOrderIsFree())
             {
-                deliveryfee = deliveryfee * 1.5;
+                deliveryfee = 0;
             }
-            //extra payment if price is low
-            if (totalPrice < 20)
+            else
             {
-                deliveryfee += 10;
+                deliveryfee = distanceceValue / 1000 / 4;
+                //check if the distance is greater than 25 km
+                //then apply additional fee
+                if (distanceceValue > 25000)
+                {
+                    deliveryfee = applyHigherTaxRate(deliveryfee);
+                }
+                else
+                {
+                    deliveryfee = applyStandartTaxRate(deliveryfee);
+                }
+
+                //extra payment if price is low
+                if (totalPrice < 20)
+                {
+                    deliveryfee = applyExtraTax(deliveryfee);
+                }
             }
-            foreach (var item in countedDishes)
-            {
-                Dish dish = await _context.Dish.FindAsync(item.id);
-                cartItems.Add(new CartViewModel(dish, dish.Price * item.count, item.count, totalPrice, (decimal)deliveryfee));
-            }
+
+            applyDeliveryPayToDishOrder(dishIds, totalPrice, deliveryfee, cartItems);
             return cartItems;
         }
+
 
         [Authorize(Roles = "User")]
         public async Task<IActionResult> Cart()
@@ -123,6 +133,54 @@ namespace FoodApp.Controllers
             HttpContext.Session.Set<List<int>>("cart", new List<int>());
 
             return Redirect("/");
+        }
+
+        bool checkIfOrderIsFree()
+        {
+            //The order is never free dumbo
+            return false; 
+        }
+
+        string getRestaurantAddress(List<int> dishIds)
+        {
+            return _context.Restaurant.FindAsync(_context.Dish.Find(dishIds.First()).RestaurantId).Result.Address;
+        }
+
+        string getUserAddress()
+        {
+            var id = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            return  _context.Client.Find(id).Address;
+        }
+
+        async Task<double> checkDistance(string client, string restaurant)
+        {
+            var distance = await _distanceProcessor.LoadDistance(client, restaurant);
+            return distance.rows.First().elements.First().distance.value;
+        }
+        private double applyHigherTaxRate(double deliveryfee)
+        {
+            return deliveryfee * 1.5;
+        }
+
+        private double applyStandartTaxRate(double deliveryfee)
+        {
+            return deliveryfee;
+        }
+
+        private double applyExtraTax(double deliveryfee)
+        {
+            return deliveryfee + 10;
+        }
+
+        private async void applyDeliveryPayToDishOrder(List<int> dishIds, decimal totalPrice, double deliveryfee, List<CartViewModel> cartItems)
+        {
+            var unique = dishIds.Distinct().ToList();
+            var countedDishes = unique.Select((id, count) => new { id, count = dishIds.Count(x => x == id) });
+            foreach (var item in countedDishes)
+            {
+                Dish dish = await _context.Dish.FindAsync(item.id);
+                cartItems.Add(new CartViewModel(dish, dish.Price * item.count, item.count, totalPrice, (decimal)deliveryfee));
+            }
         }
     }
 }
