e7ee8c5bd7be9979e0610c88a13efbf060a73f67
domgud <domantas.gudonis@ktu.edu>
2021-05-15 11:05:23 +0300

    Moved cart calculations to a seperate method

--- FoodApp/Controllers/RestaurantsController.cs
+++ FoodApp/Controllers/RestaurantsController.cs
@@ -24,6 +24,7 @@ namespace FoodApp.Controllers
         private readonly ApplicationDbContext _context;
         private readonly IFlashMessage _flashMessage;
         private readonly UserManager<Restaurant> _userManager;
+        private readonly DistanceProcessor _distanceProcessor;
 
 
         public RestaurantsController(ApplicationDbContext context, IFlashMessage flashMessage,UserManager<Restaurant> userManager)
@@ -31,6 +32,7 @@ namespace FoodApp.Controllers
             _context = context;
             _flashMessage = flashMessage;
             _userManager = userManager;
+            _distanceProcessor = new DistanceProcessor();
 
         }
         [Authorize(Roles = "Administrator")]
@@ -213,6 +215,39 @@ namespace FoodApp.Controllers
             _flashMessage.Confirmation("added to cart successfully!", _context.Dish.FindAsync(id).Result.Name);
             return Redirect(Request.Headers["Referer"].ToString());
         }
+        private async Task<List<CartViewModel>> CalculatePrice(List<int> dishIds)
+        {
+            List<CartViewModel> cartItems = new List<CartViewModel>();
+            var unique = dishIds.Distinct().ToList();
+            var countedDishes = unique.Select((id, count) => new { id, count = dishIds.Count(x => x == id) });
+            decimal totalPrice = countedDishes.Sum(item => _context.Dish.Find(item.id).Price * item.count);
+
+            //communicating with the api :^)
+            string restaurant = _context.Restaurant.FindAsync(_context.Dish.Find(dishIds.First()).RestaurantId).Result.Address;
+            var id = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            string client = _context.Client.FindAsync(id).Result.Address;
+            var distance = await _distanceProcessor.LoadDistance(client, restaurant);
+            double distanceceValue = distance.rows.First().elements.First().distance.value;
+            double deliveryfee = distanceceValue / 1000 / 4;
+            //check if the distance is greater than 25 km
+            //then apply additional fee
+            if (distanceceValue > 25000)
+            {
+                deliveryfee = deliveryfee * 1.5;
+            }
+            //extra payment if price is low
+            if (totalPrice < 20)
+            {
+                deliveryfee += 10;
+            }
+            foreach (var item in countedDishes)
+            {
+                Dish dish = await _context.Dish.FindAsync(item.id);
+                cartItems.Add(new CartViewModel(dish, dish.Price * item.count, item.count, totalPrice, (decimal)deliveryfee));
+            }
+            return cartItems;
+        }
+            
         [Authorize(Roles = "User")]
         public async Task<IActionResult> Cart()
         {
@@ -222,23 +257,10 @@ namespace FoodApp.Controllers
                 _flashMessage.Warning("Please add items to the cart first");
                 return Redirect(Request.Headers["Referer"].ToString());
             }
+
+            var cartItems = await CalculatePrice(dishIds);
             
-            var unique = dishIds.Distinct().ToList();
-            var countedDishes = unique.Select((id, count) => new { id, count = dishIds.Count(x => x == id) });
-            List<CartViewModel> cartItems = new List<CartViewModel>();
-            //communicating with the api :^)
-            string restaurant = _context.Restaurant.FindAsync(_context.Dish.Find(dishIds.First()).RestaurantId).Result.Address;
-            var id = User.FindFirstValue(ClaimTypes.NameIdentifier);
-            string client = _context.Client.FindAsync(id).Result.Address;
-            var DistanceProcessor = new DistanceProcessor();
-            var distance = await DistanceProcessor.LoadDistance(client, restaurant);
-            //move this to seperate method when doing distance calculation w google api
-            decimal totalPrice = countedDishes.Sum(item => _context.Dish.Find(item.id).Price * item.count);
-            foreach (var item in countedDishes)
-            {
-                Dish dish = await _context.Dish.FindAsync(item.id);
-                cartItems.Add(new CartViewModel(dish, dish.Price * item.count, item.count, totalPrice));
-            }
+            
             return View(cartItems);
         }
         [Authorize(Roles = "User")]
@@ -249,28 +271,25 @@ namespace FoodApp.Controllers
 
             var dishIds = HttpContext.Session.Get<List<int>>("cart");
             if (dishIds is null || dishIds.Count < 1) return Redirect("/");
-            var unique = dishIds.Distinct().ToList();
-            var countedDishes = unique.Select((id, count) => new { id, count = dishIds.Count(x => x == id) });
-            //move this to seperate method when doing distance calculation w google api
-            decimal totalPrice = countedDishes.Sum(item => _context.Dish.Find(item.id).Price * item.count);
-
+            var cartItems = await CalculatePrice(dishIds);
+            var data = cartItems.First();
             //creating the new order object
             Order order = new Order();
             order.State = Order.OrderState.Paid;
             var id = User.FindFirstValue(ClaimTypes.NameIdentifier);
             order.ClientId = id;
-            order.DeliveryFee = (double)totalPrice;
-            Dish dish = await _context.Dish.FindAsync(countedDishes.First().id);
+            order.DeliveryFee =(double) data.DeliveryFee;
+            Dish dish = await _context.Dish.FindAsync(data.Dish.Id);
             order.RestaurantId = dish.RestaurantId;
             _context.Order.Add(order);
-            foreach (var item in countedDishes)
+            foreach (var item in cartItems)
             {
                 DishOrder dishorder = new DishOrder();
-                dishorder.Dish = _context.Dish.Find(item.id);
-                dishorder.DishId = item.id;
+                dishorder.Dish = _context.Dish.Find(item.Dish.Id);
+                dishorder.DishId = item.Dish.Id;
                 dishorder.order = order;
                 dishorder.OrderId = order.Id;
-                dishorder.Amount = item.count;
+                dishorder.Amount = item.ItemCount;
                 _context.DishOrders.Add(dishorder);
             }
             await _context.SaveChangesAsync();
